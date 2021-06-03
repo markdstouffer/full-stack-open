eccdea46087218f91d00b8e594499234bb66f48e
markdstouffer <markdstouffer@gmail.com>
2021-06-03 16:04:01 -0400

    exercises 6.13-6.14

--- part6/anecdotes-redux/src/components/AnecdoteForm.js
+++ part6/anecdotes-redux/src/components/AnecdoteForm.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { notificationChange, clearNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const NewAnecdote = (props) => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
+        const newAnecdote = await anecdoteService.createAnecdote(content)
+        dispatch(createAnecdote(newAnecdote))
         dispatch(notificationChange(`new anecdote added '${content}'`))
         setTimeout(() => {
             dispatch(clearNotification())
---
+++ part6/anecdotes-redux/src/services/anecdotes.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+const createAnecdote = async (content) => {
+  const anecdoteObject = { content, votes: 0}
+  const response = await axios.post(baseUrl, anecdoteObject)
+  return response.data
+}
+
+export default { getAll, createAnecdote }
